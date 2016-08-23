c5058bb6a4f12a12267b59ef7cbb95307d6a9884
luckytianyiyan <luckytianyiyan@gmail.com>
2016-08-24 02:22:00 +0800

    M Example project

--- Example/TYUpdatePrompt/TYViewController.m
+++ Example/TYUpdatePrompt/TYViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "TYViewController.h"
+#import <TYUpdatePrompt.h>
+#import <Masonry.h>
 
 @interface TYViewController ()
 
@@ -17,13 +19,48 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [TYUpdatePrompt sharedInstance].debugEnabled = YES;
+    
+    UIButton *checkVersionButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [checkVersionButton setTitle:@"Check Version" forState:UIControlStateNormal];
+    [checkVersionButton addTarget:self action:@selector(onCheckVersionButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:checkVersionButton];
+    
+    [checkVersionButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.equalTo(self.view);
+    }];
+    
+    UILabel *debugLabel = [[UILabel alloc] init];
+    debugLabel.font = [UIFont systemFontOfSize:14.f];
+    debugLabel.text = @"Debug Switch";
+    [self.view addSubview:debugLabel];
+    
+    UISwitch *debugSwitch = [[UISwitch alloc] init];
+    debugSwitch.on = [TYUpdatePrompt sharedInstance].isDebugEnabled;
+    [debugSwitch addTarget:self action:@selector(onDebugSwitchValueChanged:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:debugSwitch];
+    
+    [debugSwitch mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.equalTo(checkVersionButton.mas_top).offset(-20.f);
+        make.left.equalTo(checkVersionButton.mas_centerX).offset(10.f);
+    }];
+    
+    [debugLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.equalTo(checkVersionButton.mas_centerX).offset(-10.f);
+        make.centerY.equalTo(debugSwitch);
+    }];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)onCheckVersionButtonClicked:(UIButton *)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSLog(@"Check Version");
+    [[TYUpdatePrompt sharedInstance] checkVersion];
+}
+
+- (void)onDebugSwitchValueChanged:(UISwitch *)sender
+{
+    NSLog(@"Debug %@", sender.isOn ? @"On" : @"Off");
+    [TYUpdatePrompt sharedInstance].debugEnabled = sender.isOn;
 }
 
 @end
